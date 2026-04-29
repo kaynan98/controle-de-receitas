394ccfb7a737c87c6a0126c8fc9499500fcb923e
kaynan98 <lucascoelho9821@gmail.com>
2026-04-29 16:09:00 -0300

    Delete src directory

--- src/pages/BlankPage.tsx
+++
@@ -1,7 +0,0 @@
-export function BlankPage() {
-  return (
-    <div className="min-h-screen bg-gray-50 p-4 max-w-4xl mx-auto">
-      <h1 className="text-3xl font-bold text-center mb-6">Página em Branco</h1>
-    </div>
-  );
-}
